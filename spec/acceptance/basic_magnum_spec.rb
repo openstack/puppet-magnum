1083fd6ef7aebacda0721e661efd5837cedf2b07
Nate Potter <nathaniel.potter@intel.com>
2016-03-17 10:35:41 -0700

    Add acceptance test
    This patch adds a basic acceptance test to the puppet-magnum
    module.
    
    Depends-On: I9047af52a02997a69e0ecce5b07470b6f59ee4cc
    Change-Id: Ia8840f1a47e07c05630a7152d43b658fd4e5ffa0

---
+++ spec/acceptance/basic_magnum_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper_acceptance'
+
+describe 'basic magnum' do
+
+  context 'default parameters' do
+
+    it 'should work with no errors' do
+      pp= <<-EOS
+      include ::openstack_integration
+      include ::openstack_integration::repos
+      include ::openstack_integration::rabbitmq
+      include ::openstack_integration::mysql
+      include ::openstack_integration::keystone
+
+      rabbitmq_vhost { '/magnum':
+        provider => 'rabbitmqctl',
+        require  => Class['rabbitmq'],
+      }
+      rabbitmq_user { 'magnum':
+        admin    => true,
+        password => 'an_even_bigger_secret',
+        provider => 'rabbitmqctl',
+        require  => Class['rabbitmq'],
+      }
+      rabbitmq_user_permissions { 'magnum@/':
+        configure_permission => '.*',
+        write_permission     => '.*',
+        read_permission      => '.*',
+        provider             => 'rabbitmqctl',
+        require              => Class['rabbitmq'],
+      }
+
+      # Magnum resources
+      class { '::magnum::keystone::auth':
+        password => 'a_big_secret',
+      }
+      class { '::magnum::db::mysql':
+        password => 'a_big_secret',
+      }
+      case $::osfamily {
+        'Debian': {
+          warning('Magnum is not yet packaged on Ubuntu systems.')
+        }
+        'RedHat': {
+          class { '::magnum': }
+          class { '::magnum::api':
+            admin_password => 'a_big_secret',
+          }
+        }
+      }
+      EOS
+
+      # Run it twice to test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    if os[:family].casecmp('RedHat') == 0
+      describe port(9511) do
+        it { is_expected.to be_listening }
+      end
+    end
+
+  end
+end
