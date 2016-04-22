ac3b678746cd814bbeef4ae4d7b08fdaf01d5af1
Nate Potter <nathaniel.potter@intel.com>
2016-04-22 14:15:11 -0700

    Update tests with rspec facts
    This patch updates tests that perform on Debian/RedHat sets to use
    on_supported_os from rspec-puppet-facts.
    
    Change-Id: I4b7bec3e0b314cda34e41bda3892847a23325bb9

--- spec/classes/magnum_logging_spec.rb
+++ spec/classes/magnum_logging_spec.rb
@@ -122,20 +122,15 @@ describe 'magnum::logging' do
       }
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      @default_facts.merge({ :osfamily => 'Debian' })
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'magnum-logging'
     end
-
-    it_configures 'magnum-logging'
   end
-
-  context 'on RedHat platforms' do
-    let :facts do
-      @default_facts.merge({ :osfamily => 'RedHat' })
-    end
-
-    it_configures 'magnum-logging'
-  end
-
 end
