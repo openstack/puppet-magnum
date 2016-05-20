d2490ccc36bc5bc70beb8acce2310530716934fa
ZhongShengping <chdzsp@163.com>
2016-05-20 21:29:03 +0800

    Add unit test for magnum::config
    The class magnum::config has no unit test,
    so add it.
    
    Change-Id: Ia0a4ddf2fd9319bdedc7bae0bce35f02dc5bf25c
    Closes-Bug: 1581282

---
+++ spec/classes/magnum_config_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'magnum::config' do
+
+  let(:config_hash) do {
+    'DEFAULT/foo' => { 'value'  => 'fooValue' },
+    'DEFAULT/bar' => { 'value'  => 'barValue' },
+    'DEFAULT/baz' => { 'ensure' => 'absent' }
+  }
+  end
+
+  shared_examples_for 'magnum_config' do
+    let :params do
+      { :magnum_config => config_hash }
+    end
+
+    it 'configures arbitrary magnum-config configurations' do
+      is_expected.to contain_magnum_config('DEFAULT/foo').with_value('fooValue')
+      is_expected.to contain_magnum_config('DEFAULT/bar').with_value('barValue')
+      is_expected.to contain_magnum_config('DEFAULT/baz').with_ensure('absent')
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'magnum_config'
+    end
+  end
+end
