75e298396e254fd093b436d4879b4ed5df10d1e6
ZhongShengping <chdzsp@163.com>
2016-11-08 10:12:36 +0800

    include ::openstacklib::defaults in params.pp
    The ::openstacklib::defaults is default configuration for all
    openstack-puppet module. This file is loaded in the params.pp
    of each class
    
    Change-Id: I70834ffa7a5b568b5e097c054332e8ea4c6dcad1

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,30 @@
+# ==Class: magnum::params
+#
 # Parameters for puppet-magnum
 #
 class magnum::params {
+  include ::openstacklib::defaults
 
   case $::osfamily {
     'RedHat': {
       # package names
-      $common_package         = 'openstack-magnum-common'
-      $api_package            = 'openstack-magnum-api'
-      $conductor_package      = 'openstack-magnum-conductor'
+      $common_package    = 'openstack-magnum-common'
+      $api_package       = 'openstack-magnum-api'
+      $conductor_package = 'openstack-magnum-conductor'
       # service names
-      $api_service            = 'openstack-magnum-api'
-      $conductor_service      = 'openstack-magnum-conductor'
-      $client_package         = 'python2-magnumclient'
+      $api_service       = 'openstack-magnum-api'
+      $conductor_service = 'openstack-magnum-conductor'
+      $client_package    = 'python2-magnumclient'
     }
     'Debian': {
       # package names
-      $common_package         = 'magnum-common'
-      $api_package            = 'magnum-api'
-      $conductor_package      = 'magnum-conductor'
+      $common_package    = 'magnum-common'
+      $api_package       = 'magnum-api'
+      $conductor_package = 'magnum-conductor'
       # service names
-      $api_service            = 'magnum-api'
-      $conductor_service      = 'magnum-conductor'
-      $client_package         = 'python-magnumclient'
+      $api_service       = 'magnum-api'
+      $conductor_service = 'magnum-conductor'
+      $client_package    = 'python-magnumclient'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
