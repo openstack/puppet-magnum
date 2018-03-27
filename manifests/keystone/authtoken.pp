211551bec5b400356557b01bb9a1fec2423042e0
ZhongShengping <chdzsp@163.com>
2018-03-27 10:33:22 +0800

    Remove deprecated keystone authtoken revocation_cache_time option
    Change-Id: I757e4a0d53afbf9ae9365b45827bc2303fcb9582

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -174,15 +174,6 @@
 #   (in seconds). Set to -1 to disable caching completely. Integer value
 #   Defaults to $::os_service_default.
 #
-# DEPRECATED PARAMETERS
-#
-# [*revocation_cache_time*]
-#   (Optional) Determines the frequency at which the list of revoked tokens is
-#   retrieved from the Identity service (in seconds). A high number of
-#   revocation events combined with a low cache duration may significantly
-#   reduce performance. Only valid for PKI tokens. Integer value
-#   Defaults to undef
-#
 class magnum::keystone::authtoken(
   $password,
   $username                       = 'magnum',
@@ -218,15 +209,9 @@ class magnum::keystone::authtoken(
   $manage_memcache_package        = false,
   $region_name                    = $::os_service_default,
   $token_cache_time               = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $revocation_cache_time          = undef,
 ) {
 
   include ::magnum::deps
-
-  if $revocation_cache_time {
-    warning('revocation_cache_time parameter is deprecated, has no effect and will be removed in the future.')
-  }
 
   keystone::resource::authtoken { 'magnum_config':
     username                       => $username,
