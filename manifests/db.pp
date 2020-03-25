de58dbfd3cc74e9ea71a834fcf68ac39e83cbb39
ZhongShengping <chdzsp@163.com>
2020-03-25 14:53:41 +0800

    Deprecate min_pool_size option
    min_pool_size option is not used，see:
    
    https://review.opendev.org/#/c/565090/
    
    Change-Id: I0d76021c3389a75e2bf7ebfef9faa11d6ffbf617
    Closes-Bug: #1868511

--- manifests/db.pp
+++ manifests/db.pp
@@ -21,10 +21,6 @@
 #   (Optional) Interval between retries of opening a database connection.
 #    Defaults to $::os_service_default
 #
-# [*database_min_pool_size*]
-#   (Optional) Minimum number of SQL connections to keep open in a pool.
-#   Defaults to $::os_service_default
-#
 # [*database_max_pool_size*]
 #   (Optional) Maximum number of SQL connections to keep open in a pool.
 #   Defaults to $::os_service_default
@@ -42,19 +38,30 @@
 #   before error is raised. Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_min_pool_size*]
+#   (Optional) Minimum number of SQL connections to keep open in a pool.
+#   Defaults to undef
+#
 class magnum::db (
   $database_connection              = 'mysql+pymysql://magnum:magnum@localhost:3306/magnum',
   $database_connection_recycle_time = $::os_service_default,
-  $database_min_pool_size           = $::os_service_default,
   $database_max_pool_size           = $::os_service_default,
   $database_max_retries             = $::os_service_default,
   $database_retry_interval          = $::os_service_default,
   $database_max_overflow            = $::os_service_default,
   $database_pool_timeout            = $::os_service_default,
   $database_db_max_retries          = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_min_pool_size           = undef,
 ) {
 
   include magnum::deps
+
+  if $database_min_pool_size {
+    warning('The database_min_pool_size parameter is deprecated, and will be removed in a future release.')
+  }
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
@@ -62,7 +69,6 @@ class magnum::db (
   oslo::db { 'magnum_config':
     connection              => $database_connection,
     connection_recycle_time => $database_connection_recycle_time,
-    min_pool_size           => $database_min_pool_size,
     max_pool_size           => $database_max_pool_size,
     max_retries             => $database_max_retries,
     retry_interval          => $database_retry_interval,
--- spec/classes/magnum_db_spec.rb
+++ spec/classes/magnum_db_spec.rb
@@ -9,7 +9,6 @@ describe 'magnum::db' do
         :db_max_retries          => '<SERVICE DEFAULT>',
         :connection              => 'mysql+pymysql://magnum:magnum@localhost:3306/magnum',
         :connection_recycle_time => '<SERVICE DEFAULT>',
-        :min_pool_size           => '<SERVICE DEFAULT>',
         :max_pool_size           => '<SERVICE DEFAULT>',
         :max_retries             => '<SERVICE DEFAULT>',
         :retry_interval          => '<SERVICE DEFAULT>',
@@ -23,7 +22,6 @@ describe 'magnum::db' do
         {
           :database_connection              => 'mysql+pymysql://magnum:magnum@localhost/magnum',
           :database_connection_recycle_time => '3601',
-          :database_min_pool_size           => '2',
           :database_max_retries             => '11',
           :database_retry_interval          => '11',
           :database_max_pool_size           => '11',
@@ -39,7 +37,6 @@ describe 'magnum::db' do
         :db_max_retries          => '-1',
         :connection              => 'mysql+pymysql://magnum:magnum@localhost/magnum',
         :connection_recycle_time => '3601',
-        :min_pool_size           => '2',
         :max_pool_size           => '11',
         :max_retries             => '11',
         :retry_interval          => '11',
