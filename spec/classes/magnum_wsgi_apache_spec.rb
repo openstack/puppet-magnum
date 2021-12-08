d9224f45f9f4f5d8f75f7625212b1b5264fabb5c
Takashi Kajinami <tkajinam@redhat.com>
2021-12-08 22:29:46 +0900

    Add missing unit tests for magnum::wsgi::apache
    Change-Id: Ia497be6d11f6ada274f6b66985e31f4a346d595b

---
+++ spec/classes/magnum_wsgi_apache_spec.rb
@@ -0,0 +1,109 @@
+require 'spec_helper'
+
+describe 'magnum::wsgi::apache' do
+
+  shared_examples_for 'apache serving magnum with mod_wsgi' do
+    context 'with default parameters' do
+      it { is_expected.to contain_class('magnum::params') }
+      it { is_expected.to contain_class('apache') }
+      it { is_expected.to contain_class('apache::mod::wsgi') }
+      it { is_expected.to_not contain_class('apache::mod::ssl') }
+      it { is_expected.to contain_openstacklib__wsgi__apache('magnum_wsgi').with(
+        :bind_port                   => 9511,
+        :group                       => 'magnum',
+        :path                        => '/',
+        :servername                  => facts[:fqdn],
+        :ssl                         => false,
+        :threads                     => 1,
+        :user                        => 'magnum',
+        :workers                     => facts[:os_workers],
+        :wsgi_daemon_process         => 'magnum',
+        :wsgi_process_group          => 'magnum',
+        :wsgi_script_dir             => platform_params[:wsgi_script_path],
+        :wsgi_script_file            => 'app',
+        :wsgi_script_source          => platform_params[:wsgi_script_source],
+        :custom_wsgi_process_options => {},
+        :access_log_file             => false,
+        :access_log_format           => false,
+      )}
+    end
+
+    context 'when overriding parameters using different ports' do
+      let :params do
+        {
+          :servername                  => 'dummy.host',
+          :bind_host                   => '10.42.51.1',
+          :port                        => 12345,
+          :ssl                         => true,
+          :wsgi_process_display_name   => 'magnum',
+          :workers                     => 37,
+          :custom_wsgi_process_options => {
+            'python_path' => '/my/python/path',
+          },
+          :access_log_file             => '/var/log/httpd/access_log',
+          :access_log_format           => 'some format',
+          :error_log_file              => '/var/log/httpd/error_log',
+        }
+      end
+      it { is_expected.to contain_class('magnum::params') }
+      it { is_expected.to contain_class('apache') }
+      it { is_expected.to contain_class('apache::mod::wsgi') }
+      it { is_expected.to contain_class('apache::mod::ssl') }
+      it { is_expected.to contain_openstacklib__wsgi__apache('magnum_wsgi').with(
+        :bind_host                 => '10.42.51.1',
+        :bind_port                 => 12345,
+        :group                     => 'magnum',
+        :path                      => '/',
+        :servername                => 'dummy.host',
+        :ssl                       => true,
+        :threads                   => 1,
+        :user                      => 'magnum',
+        :workers                   => 37,
+        :wsgi_daemon_process       => 'magnum',
+        :wsgi_process_display_name => 'magnum',
+        :wsgi_process_group        => 'magnum',
+        :wsgi_script_dir           => platform_params[:wsgi_script_path],
+        :wsgi_script_file          => 'app',
+        :wsgi_script_source        => platform_params[:wsgi_script_source],
+        :custom_wsgi_process_options => {
+          'python_path' => '/my/python/path',
+        },
+        :access_log_file           => '/var/log/httpd/access_log',
+        :access_log_format         => 'some format',
+        :error_log_file            => '/var/log/httpd/error_log'
+      )}
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts({
+          :os_workers     => 8,
+          :concat_basedir => '/var/lib/puppet/concat',
+          :fqdn           => 'some.host.tld'
+        }))
+      end
+
+      let(:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          {
+            :wsgi_script_path   => '/usr/lib/cgi-bin/magnum',
+            :wsgi_script_source => '/usr/lib/python3/dist-packages/magnum/api/app.wsgi'
+          }
+        when 'RedHat'
+          {
+            :wsgi_script_path   => '/var/www/cgi-bin/magnum',
+            :wsgi_script_source => '/usr/bin/magnum-api-wsgi'
+          }
+
+        end
+      end
+      it_configures 'apache serving magnum with mod_wsgi'
+    end
+  end
+
+end
