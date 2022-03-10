d4b442f6298e56dcbc051ee7b00c5fd829c1da7a
Takashi Kajinami <tkajinam@redhat.com>
2022-03-10 16:09:14 +0900

    Fix unit tests for magnum::clients::heat class
    The spec file for magnum::clients::heat was testing the wrong class
    actually. This fixes that and ensures the heat class is tested.
    
    Change-Id: I3fd53c45beb70e3d7a270625cdbb023d0dc94ea6

--- spec/classes/magnum_clients_heat_spec.rb
+++ spec/classes/magnum_clients_heat_spec.rb
@@ -1,29 +1,48 @@
 require 'spec_helper'
 
-describe 'magnum::clients::barbican' do
+describe 'magnum::clients::heat' do
 
-  shared_examples 'magnum::clients::barbican' do
+  shared_examples 'magnum::clients::heat' do
 
     context 'with default parameters' do
       let :params do
         { :region_name     => 'RegionOne',
           :endpoint_type   => 'publicURL',
+          :ca_file         => '<SERVICE DEFAULT>',
+          :cert_file       => '<SERVICE DEFAULT>',
+          :key_file        => '<SERVICE DEFAULT>',
+          :insecure        => false,
         }
       end
 
-      it { is_expected.to contain_magnum_config('barbican_client/region_name').with_value('RegionOne') }
-      it { is_expected.to contain_magnum_config('barbican_client/endpoint_type').with_value('publicURL') }
+      it { is_expected.to contain_magnum_config('heat_client/region_name').with_value('RegionOne') }
+      it { is_expected.to contain_magnum_config('heat_client/endpoint_type').with_value('publicURL') }
+      it { is_expected.to contain_magnum_config('heat_client/api_version').with_value('1') }
+      it { is_expected.to contain_magnum_config('heat_client/ca_file').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_magnum_config('heat_client/cert_file').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_magnum_config('heat_client/key_file').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_magnum_config('heat_client/insecure').with_value(false) }
     end
 
     context 'with specific parameters' do
       let :params do
         { :region_name     => 'RegionTwo',
           :endpoint_type   => 'adminURL',
+          :api_version     => '2',
+          :ca_file         => '/etc/magnum/certs/ca.pem',
+          :cert_file       => '/etc/magnum/certs/cert.pem',
+          :key_file        => '/etc/magnum/certs/pri.key',
+          :insecure        => true,
         }
       end
 
-      it { is_expected.to contain_magnum_config('barbican_client/region_name').with_value('RegionTwo') }
-      it { is_expected.to contain_magnum_config('barbican_client/endpoint_type').with_value('adminURL') }
+      it { is_expected.to contain_magnum_config('heat_client/region_name').with_value('RegionTwo') }
+      it { is_expected.to contain_magnum_config('heat_client/endpoint_type').with_value('adminURL') }
+      it { is_expected.to contain_magnum_config('heat_client/api_version').with_value('2') }
+      it { is_expected.to contain_magnum_config('heat_client/ca_file').with_value('/etc/magnum/certs/ca.pem') }
+      it { is_expected.to contain_magnum_config('heat_client/cert_file').with_value('/etc/magnum/certs/cert.pem') }
+      it { is_expected.to contain_magnum_config('heat_client/key_file').with_value('/etc/magnum/certs/pri.key') }
+      it { is_expected.to contain_magnum_config('heat_client/insecure').with_value(true) }
     end
   end
 
@@ -35,7 +54,7 @@ describe 'magnum::clients::barbican' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'magnum::clients::barbican'
+      it_configures 'magnum::clients::heat'
     end
   end
 end
