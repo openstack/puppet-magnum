ec182eb7c1a839f27181b66e0e57b92e99f398b1
Takashi Kajinami <tkajinam@redhat.com>
2022-09-07 00:32:02 +0900

    Enable memcached in acceptance tests
    ... because it is required as cache backend.
    
    Change-Id: I405ed32b07e9d485c1807c2af2410166f5c678e8

--- spec/acceptance/10_basic_magnum_spec.rb
+++ spec/acceptance/10_basic_magnum_spec.rb
@@ -11,6 +11,7 @@ describe 'basic magnum' do
       include openstack_integration::apache
       include openstack_integration::rabbitmq
       include openstack_integration::mysql
+      include openstack_integration::memcached
       include openstack_integration::keystone
 
       class { 'openstack_integration::magnum':
