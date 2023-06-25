26844c2f79c7c9e2c4fc53848c6eebb0c1248de2
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:52:14 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: Ied6c7759af242021ed83e3c60197e74c947bf811
    (cherry picked from commit f42642bd61205cb8ed6968ae9cf2180eb772099a)
    (cherry picked from commit 93c2557cac8bd7166cc03797e734b7b4bd824d5a)
    (cherry picked from commit 2a046d22f62665fbca694502cf0943f1716c815d)

--- spec/classes/magnum_policy_spec.rb
+++ spec/classes/magnum_policy_spec.rb
@@ -32,6 +32,7 @@ describe 'magnum::policy' do
           :file_group   => 'magnum',
           :file_format  => 'yaml',
           :purge_config => false,
+          :tag          => 'magnum',
         )
         is_expected.to contain_oslo__policy('magnum_config').with(
           :enforce_scope        => false,
@@ -61,6 +62,7 @@ describe 'magnum::policy' do
           :file_group   => 'magnum',
           :file_format  => 'yaml',
           :purge_config => true,
+          :tag          => 'magnum',
         )
         is_expected.to contain_oslo__policy('magnum_config').with(
           :enforce_scope        => false,
