6422bbef82bfb62dcf26c5b19beade533f335ba0
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-07-25 22:45:54 +0900

    Add support for [healthcheck] enable_by_file_paths
    Depends-on: https://review.opendev.org/923648
    Change-Id: I10747e111cf1c9ba4a880173a58abdaa9619943d

--- spec/classes/magnum_healthcheck_spec.rb
+++ spec/classes/magnum_healthcheck_spec.rb
@@ -17,6 +17,7 @@ describe 'magnum::healthcheck' do
           :ignore_proxied_requests => '<SERVICE DEFAULT>',
           :disable_by_file_path    => '<SERVICE DEFAULT>',
           :disable_by_file_paths   => '<SERVICE DEFAULT>',
+          :enable_by_file_paths    => '<SERVICE DEFAULT>',
         )
       end
     end
@@ -30,6 +31,7 @@ describe 'magnum::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/magnum/healthcheck/disabled',
           :disable_by_file_paths   => ['9511:/etc/magnum/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/magnum/healthcheck/enabled'],
         }
       end
 
@@ -41,6 +43,7 @@ describe 'magnum::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/magnum/healthcheck/disabled',
           :disable_by_file_paths   => ['9511:/etc/magnum/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/magnum/healthcheck/enabled'],
         )
       end
     end
