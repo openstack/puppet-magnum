9e882736142023e12729bd2b5cf79b6f4e7f3bc7
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-10-12 12:15:13 +0900

    Expose configure_service
    This parameter has been implemented in the underlying defined resource
    type but was not configurable.
    
    Change-Id: I8a9149ec7c43437f2392bcce39967e73c118dd34

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -43,6 +43,10 @@
 #   (Optional) Should the admin role be configured for the service user?
 #   Defaults to true.
 #
+# [*configure_service*]
+#   (Optional) Should the service be configurd?
+#   Defaults to True
+#
 # [*service_type*]
 #   (Optional) Type of service.
 #   Defaults to 'container-infra'.
@@ -84,6 +88,7 @@ class magnum::keystone::auth (
   Boolean $configure_endpoint             = true,
   Boolean $configure_user                 = true,
   Boolean $configure_user_role            = true,
+  Boolean $configure_service              = true,
   String[1] $service_description          = 'magnum Container Service',
   Optional[String[1]] $service_name       = undef,
   String[1] $service_type                 = 'container-infra',
@@ -103,6 +108,7 @@ class magnum::keystone::auth (
     configure_user      => $configure_user,
     configure_user_role => $configure_user_role,
     configure_endpoint  => $configure_endpoint,
+    configure_service   => $configure_service,
     service_name        => $real_service_name,
     service_type        => $service_type,
     service_description => $service_description,
