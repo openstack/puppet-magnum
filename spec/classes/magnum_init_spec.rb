165d6656911056db5891ace29edaaa4473e6dc71
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-02-20 09:50:26 +0900

    Fix assertion of heartbeat_in_pthread parameter
    Its default in puppet-oslo has been changed by [1].
    
    [1] 06a9b12199ed03842bb935749dedf4fc8e20d043
    
    Change-Id: I19c791da17cac3256161c6b80fd7655073cf9482

--- spec/classes/magnum_init_spec.rb
+++ spec/classes/magnum_init_spec.rb
@@ -35,7 +35,7 @@ describe 'magnum' do
           :rabbit_ha_queues                => '<SERVICE DEFAULT>',
           :heartbeat_timeout_threshold     => '<SERVICE DEFAULT>',
           :heartbeat_rate                  => '<SERVICE DEFAULT>',
-          :heartbeat_in_pthread            => '<SERVICE DEFAULT>',
+          :heartbeat_in_pthread            => nil,
           :rabbit_qos_prefetch_count       => '<SERVICE DEFAULT>',
           :rabbit_use_ssl                  => '<SERVICE DEFAULT>',
           :kombu_reconnect_delay           => '<SERVICE DEFAULT>',
