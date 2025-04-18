3e32b16bf49d7daade2a02fcb7fe657d00644f2c
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-04-18 12:09:38 +0900

    Remove conductor_life_check_timeout
    The parameter was deprecated in the past cycle[1] and has had no effect
    since then.
    
    [1] 4feba93488a4757d0ba8b448bc73ae532eb7b924
    
    Change-Id: I927492f4b0ccb8d6bc07c3fcf71a80df0351392b

--- manifests/conductor.pp
+++ manifests/conductor.pp
@@ -24,31 +24,17 @@
 #   (optional) Number of conductor workers.
 #   Defaults to $facts['os_workers']
 #
-# DEPRECATED PARAMETERS
-#
-# [*conductor_life_check_timeout*]
-#   (optional) RPC timeout for the conductor liveness check that is
-#    used for bay locking.
-#    Defaults to undef
-#
 class magnum::conductor(
   Boolean $enabled              = true,
   Boolean $manage_service       = true,
   $package_ensure               = 'present',
   $auth_strategy                = 'keystone',
   $workers                      = $facts['os_workers'],
-  # DEPRECATED PARAMETERS
-  $conductor_life_check_timeout = undef,
 ) {
 
   include magnum::db
   include magnum::deps
   include magnum::params
-
-  if $conductor_life_check_timeout != undef {
-    warning("The conductor_life_check_timeout parameter has been deprecated \
-and has no effect.")
-  }
 
   # Install package
   package { 'magnum-conductor':
@@ -76,11 +62,6 @@ and has no effect.")
 
   magnum_config {
     'conductor/workers': value => $workers;
-  }
-
-  # Remove this and conductor_life_check_timeout after 2025.1 release
-  magnum_config {
-    'conductor/conductor_life_check_timeout': ensure => absent;
   }
 
   if $auth_strategy == 'keystone' {
