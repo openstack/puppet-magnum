0af52391447d41a6ec13bb89fe64c23813cfb47f
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-07-21 03:06:39 +0900

    domain: Deprecate _id parameters
    Using names is the preferred approach because we can use predictable
    values.
    
    Change-Id: Iba27be562e9ba12eed8b14d59f6f7b8e66112d0e
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/keystone/domain.pp
+++ manifests/keystone/domain.pp
@@ -20,17 +20,9 @@
 #   Keystone domain admin user which will be created.
 #   Defaults to 'magnum_admin'.
 #
-# [*domain_admin_id*]
-#   Id of the admin with roles sufficient to manage users in the trustee_domain.
-#   Defaults to $facts['os_service_default'].
-#
 # [*domain_admin_domain_name*]
 #   Name of the domain admin user's domain.
 #   Defaults to $domain_name.
-#
-# [*domain_admin_domain_id*]
-#   Id of the domain admin user's domain.
-#   Defaults to $facts['os_service_default'].
 #
 # [*domain_admin_email*]
 #   Keystone domain admin user email address.
@@ -68,26 +60,44 @@
 #   Whether manage or not the user role creation.
 #   Defaults to 'true'.
 #
+# DEPRECATED PARAMETERS
+#
+# [*domain_admin_id*]
+#   Id of the admin with roles sufficient to manage users in the trustee_domain.
+#   Defaults to $facts['os_service_default'].
+#
+# [*domain_admin_domain_id*]
+#   Id of the domain admin user's domain.
+#   Defaults to $facts['os_service_default'].
+#
 class magnum::keystone::domain (
   $cluster_user_trust       = $facts['os_service_default'],
   $domain_name              = 'magnum',
   $domain_id                = $facts['os_service_default'],
   $domain_admin             = 'magnum_admin',
-  $domain_admin_id          = $facts['os_service_default'],
   $domain_admin_email       = 'magnum_admin@localhost',
   $domain_password          = undef,
   $domain_admin_domain_name = $facts['os_service_default'],
-  $domain_admin_domain_id   = $facts['os_service_default'],
   $roles                    = $facts['os_service_default'],
   $keystone_interface       = 'public',
   $keystone_region_name     = $facts['os_service_default'],
   Boolean $manage_domain    = true,
   Boolean $manage_user      = true,
   Boolean $manage_role      = true,
+  # DEPRECATED PARAMETERS
+  $domain_admin_id          = undef,
+  $domain_admin_domain_id   = undef,
 ) {
 
   include magnum::deps
   include magnum::params
+
+  if $domain_admin_id != undef {
+    warning('The domain_admin_id parameter is deprecated')
+  }
+  if $domain_admin_domain_id != undef {
+    warning('The domain_admin_domain_id parameter is deprecated')
+  }
 
   if $manage_domain {
     ensure_resource('keystone_domain', $domain_name, {
@@ -115,6 +125,8 @@ class magnum::keystone::domain (
     })
   }
 
+  $domain_admin_id_real = pick($domain_admin_id, $facts['os_service_default'])
+  $domain_admin_domain_id_real = pick($domain_admin_domain_id, $facts['os_service_default'])
   $domain_password_real = pick($domain_password, $facts['os_service_default'])
 
   magnum_config {
@@ -122,9 +134,9 @@ class magnum::keystone::domain (
     'trust/trustee_domain_name':               value => $domain_name;
     'trust/trustee_domain_id':                 value => $domain_id;
     'trust/trustee_domain_admin_name':         value => $domain_admin;
-    'trust/trustee_domain_admin_id':           value => $domain_admin_id;
+    'trust/trustee_domain_admin_id':           value => $domain_admin_id_real;
     'trust/trustee_domain_admin_domain_name':  value => $domain_admin_domain_name;
-    'trust/trustee_domain_admin_domain_id':    value => $domain_admin_domain_id;
+    'trust/trustee_domain_admin_domain_id':    value => $domain_admin_domain_id_real;
     'trust/trustee_domain_admin_password':     value => $domain_password_real, secret => true;
     'trust/roles':                             value => join(any2array($roles), ',');
     'trust/trustee_keystone_interface':        value => $keystone_interface;
