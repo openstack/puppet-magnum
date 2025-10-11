f01fb251fa9dc51f29a7a47cfc549ae93b3ecc2e
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-10-11 11:59:02 +0900

    Add service role by default
    This is the role created to be used by service users. At this moment
    several services/functionalities still need the admin role so is kept.
    
    Change-Id: I2a34338518f0891030a38353fb8a2a265bd1664c
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -21,7 +21,7 @@
 #
 # [*roles*]
 #   (Optional) List of roles assigned to magnum user.
-#   Defaults to ['admin']
+#   Defaults to ['admin', 'service']
 #
 # [*system_scope*]
 #   (Optional) Scope for system operations.
@@ -82,7 +82,7 @@ class magnum::keystone::auth (
   String[1] $auth_name                    = 'magnum',
   String[1] $email                        = 'magnum@localhost',
   String[1] $tenant                       = 'services',
-  Array[String[1]] $roles                 = ['admin'],
+  Array[String[1]] $roles                 = ['admin', 'service'],
   String[1] $system_scope                 = 'all',
   Array[String[1]] $system_roles          = [],
   Boolean $configure_endpoint             = true,
